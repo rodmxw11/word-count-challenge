5074a55f8757716b2b0871b3e9f11343093f2052
RODNEY Maxwell <rmaxwell@merative.com>
2022-07-25 15:54:15 -0500

    Works using Java Streams

--- src/main/java/org/rodney/WordCountChallenge.java
+++ src/main/java/org/rodney/WordCountChallenge.java
@@ -3,16 +3,33 @@ package org.rodney;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WordCountChallenge {
-    private static final String input_file = "src/main/resources/kjvbible.htm";
+    private static final String input_file = "src/main/resources/kjvbible.txt";
     public static void main(String[] args) {
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(input_file))) {
 
-            long line_count =  stream.count();
-            System.out.println("line_count="+line_count);
+            Map<String,Long> word_counts = stream
+                    .flatMap(line -> Arrays.stream(line.split("\\s+")))
+                    .map(String::toLowerCase)
+                            .collect(
+                                    Collectors.groupingBy(
+                                            Function.identity(),
+                                            Collectors.counting()
+                                            ));
+            word_counts.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(20)
+                    .forEach(entry -> System.out.println(
+                            String.format("%8d  %s",entry.getValue(), entry.getKey())
+                    ));
 
         } catch (IOException e) {
             e.printStackTrace();
